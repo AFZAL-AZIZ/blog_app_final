7be7bbdf65a5dc14f25fb7ec6536d68460252ce8
Afzal Aziz <afzalaziz01@gmail.com>
2022-06-06 11:43:47 +0000

    updated html

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   
   def hello
-    render html: "hello, world!"
+    render html: "Bye, world. I am going to take this website down for few hours"
   end
 end
